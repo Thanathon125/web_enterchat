d3ce86d59df6db4330d1c28556b51797d49946aa
Ryuukha <thanathonch@entercorp.net>
2021-08-15 15:38:53 +0700

    first commit

---
+++ web_enterchat/src/main/java/net/entercorp/web_enterchat/WebenterchatApplication.java
@@ -0,0 +1,13 @@
+package net.entercorp.web_enterchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebenterchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebenterchatApplication.class, args);
+	}
+
+}
---
+++ web_enterchat/src/test/java/net/entercorp/web_enterchat/WebenterchatApplicationTests.java
@@ -0,0 +1,13 @@
+package net.entercorp.web_enterchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebenterchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
